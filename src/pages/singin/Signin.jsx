52a419eb425d1c5fd6dde432039cff20f65562cb
iibrahim70 <aviknuhash70@gmail.com>
2023-06-07 15:25:55 +0600

    impletend signup and signin and signout functionality

--- src/pages/singin/Signin.jsx
+++ src/pages/singin/Signin.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Lottie from 'lottie-react';
 import signinAnimation from '../../assets/animation/register/signin.json';
 import { Link } from 'react-router-dom';
-
+import { BsEyeFill, BsEyeSlashFill } from 'react-icons/bs';
+import useAuth from '../../hooks/useAuth';
 
 const Signin = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { Login } = useAuth();  
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const onSubmit = user => {
+    const {email, password} = user; 
+    Login(email, password)
+      .then(res => {
+        const user = res.user; 
+        console.log(user);
+      })
+      .catch(err => console.error(err));
   };
 
   return (
@@ -24,54 +34,52 @@ const Signin = () => {
 
       <div className='flex-1'>
         <form className='shadow-xl p-10' onSubmit={handleSubmit(onSubmit)}>
+          
           <div className="mb-4">
             <label className="block mb-1 font-medium">Email</label>
-            <input
-              className="w-full border-b border-[#212121] py-2 px-3 focus:outline-none focus:border-[#2ECC71] focus:ring-2 focus:ring-[#bg-gradient-to-r from-transparent via-lime-700 to-cyan-600]"
-              {...register('email', { required: true })}
-            />
+            <input className="w-full border-b border-[#212121] py-2 px-3 focus:outline-none focus:border-[#2ECC71] focus:ring-2 focus:ring-[#bg-gradient-to-r from-transparent via-lime-700 to-cyan-600]" {...register('email', { required: true })}/>
             {errors.email && (
-              <span className="text-red-500 text-sm">This field is required</span>
+              <span className="text-red-500 text-sm">
+                Enter a valid Email
+              </span>
             )}
           </div>
 
           <div className="mb-4">
             <label className="block mb-1 font-medium">Password</label>
             <div className="relative">
-              <input
-                type="password"
-                className="w-full border-b border-[#212121] py-2 pl-3 pr-10 focus:outline-none focus:border-[#2ECC71] focus:ring-2 focus:ring-[#bg-gradient-to-r from-transparent via-lime-700 to-cyan-600]"
-                {...register('password', { required: true })}
-              />
+              <input type={showPassword ? 'text' : 'password'}className="w-full border-b border-[#212121] py-2 pl-3 pr-10 focus:outline-none focus:border-[#2ECC71] focus:ring-2 focus:ring-[#bg-gradient-to-r from-transparent via-lime-700 to-cyan-600]" {...register('password', { required: true })}/>
               <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
-                <button
-                  type="button"
-                  onClick={() => {
-                    // Toggle password visibility
-                  }}
-                >
-                  {/* Add an icon here to toggle password visibility */}
+                <button type="button" onClick={togglePasswordVisibility}>
+                  {showPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
                 </button>
               </div>
             </div>
+
             {errors.password && (
-              <span className="text-red-500 text-sm">This field is required</span>
+              <span className="text-red-500 text-sm">
+                Enter a valid Password
+              </span>
             )}
           </div>
 
-          <div className="mb-4">
-            <Link to="/signup" className="text-blue-500">Create an account</Link>
-          </div>
-
-          <div className="mb-4">
-            {/* Add a social login button */}
-          </div>
-
-          <button type="submit" className="btn btn-primary">Login</button>
+          <input type="submit" className="btn btn-primary" value='Login'/>
         </form>
+
+        <div className="mb-4">
+          <Link to="/signup" className="text-blue-500">Create an account</Link>
+        </div>
+
+        <div className="mb-4">
+          {/* Add a social login button */}
+        </div>
+
       </div>
     </div>
   );
 };
 
 export default Signin;
+
+
+
