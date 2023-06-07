64269822195405b25fa325b82685158c4276b20e
iibrahim70 <aviknuhash70@gmail.com>
2023-06-07 10:03:14 +0600

    add the signin page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,6 +4,7 @@ import Main from "../layout/Main";
 import Home from "../pages/home/Home";
 import ErrorPage from "../pages/errorpage/ErrorPage";
 import Signup from "../pages/signup/Signup";
+import Signin from "../pages/singin/Signin";
 
 
 export const router = createBrowserRouter([
@@ -19,6 +20,10 @@ export const router = createBrowserRouter([
       {
         path: '/signup',
         element: <Signup />,
+      },
+      {
+        path: '/signin',
+        element: <Signin />,
       },
     ]
   }
