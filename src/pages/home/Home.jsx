17b7a6d97baf787bd6e07f22c56490edd76438d3
iibrahim70 <aviknuhash70@gmail.com>
2023-06-11 09:53:43 +0600

    added hero section of the webpage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Hero from '../../components/hero/Hero';
 import Instructors from '../instructors/Instructors';
+import MyCarousel from '../../components/mycarousel/MyCarousel';
+import Hero from '../../components/hero/Hero';
 
 const Home = () => {
   return (
     <>
       <Hero/>
+      <MyCarousel/>
       <Instructors/>
     </>
   );
--- src/pages/instructors/Instructors.jsx
+++ src/pages/instructors/Instructors.jsx
@@ -12,7 +12,7 @@ const Instructors = () => {
     </div>
   }
   return (
-    <div className='grid grid-cols-3 gap-10 my-20'>
+    <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-10 my-20'>
       {instructors.map(item => (
         <div key={item._id} className="shadow-xl">
           <figure>
