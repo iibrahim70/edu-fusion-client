c55338c3007b82d1ab4020ee1c8c57c3d4177e33
iibrahim70 <iibrahiim.dev@gmail.com>
2023-09-02 22:51:45 +0600

    added section title and description component

--- src/components/sectiontitle/SectionTitle.jsx
+++ src/components/sectiontitle/SectionTitle.jsx
@@ -1,9 +1,10 @@
-const Title = ({ title }) => {
+const SectionTitle = ({ title, description }) => {
   return (
-    <div>
-      <h2>{title}</h2>
+    <div className="space-y-5 mb-10">
+      <h2 className="text-3xl xl:text-4xl font-bold">{title}</h2>
+      <p className="text-justify xl:w-[70%]">{description}</p>
     </div>
   );
 };
 
-export default Title;
+export default SectionTitle;
