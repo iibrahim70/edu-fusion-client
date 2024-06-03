130b9063ce47d28dd8eb75557963cab9ba301151
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-03 22:19:36 +0600

    added signin form

--- src/components/forms/SigninFrom.tsx
+++ src/components/forms/SigninFrom.tsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
+
 const SigninFrom = () => {
-  return <div>SigninFrom</div>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <form className="space-y-3.5" onSubmit={handleSubmit(onSubmit)}>
+      <div className="space-y-2.5">
+        <Label className="font-medium">Email</Label>
+        <Input type="email" {...register("email", { required: true })} />
+        {errors?.name && (
+          <span className="text-red-500 text-sm">Enter a valid Email</span>
+        )}
+      </div>
+
+      <div className="space-y-2.5">
+        <Label className="font-medium">Password</Label>
+        <div className="relative">
+          <Input
+            type={showPassword ? "text" : "password"}
+            {...register("password", { required: true })}
+          />
+
+          <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
+            <button onClick={() => setShowPassword((prev) => !prev)}>
+              {showPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
+            </button>
+          </div>
+        </div>
+
+        {errors?.password && (
+          <span className="text-red-500 text-sm">Enter a valid Password</span>
+        )}
+      </div>
+
+      <Button type="submit" className="w-full">
+        Signin
+      </Button>
+    </form>
+  );
 };
 
 export default SigninFrom;
--- src/pages/auth/Signin.tsx
+++ src/pages/auth/Signin.tsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 import useAuth from "../../hooks/useAuth";
-import { Button } from "@/components/ui/button";
 import SocialLogin from "@/components/SocialLogin";
-// import SocialLogin from "../../components/sociallogin/SocialLogin";
-// import useToast from "../../hooks/useToast";
-// import Button from "../../components/button/Button";
+import useToast from "@/hooks/useToast";
+import SigninFrom from "@/components/forms/SigninFrom";
 
 const Signin = () => {
   const { Login } = useAuth();
@@ -22,11 +18,6 @@ const Signin = () => {
     formState: { errors },
   } = useForm();
 
-  const [showPassword, setShowPassword] = useState(false);
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
   const onSubmit = (user) => {
     Login(user.email, user.password)
       .then((res) => {
@@ -40,55 +31,18 @@ const Signin = () => {
   };
 
   return (
-    <main className="flex justify-center items-center min-h-screen">
-      <div className="shadow-xl p-10 w-[90%] md:w-[60%] xl:w-[50%] mx-auto">
-        <h2 className="text-center text-4xl font-bold mb-10">Signin</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="mb-4">
-            <label className="block mb-1 font-medium">Email</label>
-            <input
-              className="inputFiedls"
-              {...register("email", { required: true })}
-            />
-            {errors.email && (
-              <span className="text-red-500 text-sm">Enter a valid Email</span>
-            )}
-          </div>
+    <main className="min-h-screen flex justify-center items-center">
+      <div className="shadow rounded-md border p-8 w-full max-w-md mx-auto space-y-3.5">
+        <h3 className="text-center">Signin</h3>
+        <SigninFrom />
 
-          <div className="mb-4">
-            <label className="block mb-1 font-medium">Password</label>
-            <div className="relative">
-              <input
-                type={showPassword ? "text" : "password"}
-                className="inputFiedls"
-                {...register("password", { required: true })}
-              />
-              <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
-                <button type="button" onClick={togglePasswordVisibility}>
-                  {showPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
-                </button>
-              </div>
-            </div>
+        <SocialLogin />
 
-            {errors.password && (
-              <span className="text-red-500 text-sm">
-                Enter a valid Password
-              </span>
-            )}
-          </div>
-
-          <Button type="submit">Signin</Button>
-        </form>
-
-        {/* toggle singup */}
-        <div className="space-y-4 mt-4 flex flex-col items-center justify-center">
-          <SocialLogin />
-          <p>
-            Don't Have An Account Yet?{" "}
-            <Link to="/signup" className="text-red-500">
-              Signup
-            </Link>
-          </p>
+        <div className="flex items-center gap-x-1.5">
+          <p>Don't Have An Account Yet?</p>
+          <Link to="/signup" className="text-blue-500">
+            Signup
+          </Link>
         </div>
       </div>
     </main>
