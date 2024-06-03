0df54285eae0a8226b6e65647c7ff7a516d5befd
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-03 13:46:48 +0600

    update nav

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./providers/authProvider";
-import router from "./routes/routes";
 import { RouterProvider } from "react-router-dom";
-import "./global.css";
 import { ThemeProvider } from "./providers/themeProvider";
+import router from "./routes/routes";
+import "./global.css";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <ThemeProvider>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <AuthProvider>
           <RouterProvider router={router} />
-        </ThemeProvider>
-      </AuthProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
