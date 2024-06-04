55e93f14d769991832913b221eb9f40c932d70a9
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-04 12:33:46 +0600

    update hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,17 +3,19 @@ import { buttonVariants } from "./ui/button";
 
 const Hero = () => {
   return (
-    <section className="section-wrapper min-h-[calc(100dvh-64px)] flex flex-col items-center justify-center gap-5 text-center">
-      <h1>Empowering Minds, Connecting Learners</h1>
-      <p>
-        "Empowering Minds, Connecting Learners" - Transform your educational
-        journey at EduFusion, where students, tutors, and administrators come
-        together to inspire and achieve academic excellence. Join us and be a
-        part of a dynamic learning community.
-      </p>
-      <Link to="/classes" className={buttonVariants()}>
-        Get Classes
-      </Link>
+    <section className="hero-banner bg-fixed min-h-[calc(100dvh-64px)] flex items-center justify-center">
+      <div className="section-wrapper text-center text-white/90 space-y-5">
+        <h1>Empowering Minds, Connecting Learners</h1>
+        <p>
+          "Empowering Minds, Connecting Learners" - Transform your educational
+          journey at EduFusion, where students, tutors, and administrators come
+          together to inspire and achieve academic excellence. Join us and be a
+          part of a dynamic learning community.
+        </p>
+        <Link to="/classes" className={buttonVariants()}>
+          Get Classes
+        </Link>
+      </div>
     </section>
   );
 };
