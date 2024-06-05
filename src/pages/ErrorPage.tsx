09980e350283b88654fbe9068d87f506d293921d
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-05 23:33:56 +0600

    remove some animations

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,5 @@
 import Lottie from "lottie-react";
-import errorAnimation from "../assets/animation/errorpage/2.json";
+import errorAnimation from "../assets/animation/error.json";
 import { Link, useRouteError } from "react-router-dom";
 import { buttonVariants } from "@/components/ui/button";
 
@@ -7,8 +7,12 @@ const ErrorPage = () => {
   const { error } = useRouteError();
 
   return (
-    <main className="section-wrapper flex flex-col items-center justify-center space-y-5">
-      <Lottie animationData={errorAnimation} loop={true} />
+    <main className="section-wrapper min-h-screen flex flex-col items-center justify-center space-y-5">
+      <Lottie
+        animationData={errorAnimation}
+        loop={true}
+        className="size-[40%]"
+      />
 
       <h4 className="font-medium">Opps!! {error?.message}</h4>
 
