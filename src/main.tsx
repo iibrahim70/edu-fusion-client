d04f6fe186c98182d98bf88c2057de2610b51504
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-10 19:05:04 +0600

    :sparkles: added rtk query

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./providers/authProvider";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./providers/themeProvider";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import { Toaster } from "react-hot-toast";
 import router from "./routes/routes";
 import "./global.css";
@@ -12,13 +14,15 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <AuthProvider>
-          <Toaster position="top-right" />
-          <RouterProvider router={router} />
-        </AuthProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+          <AuthProvider>
+            <Toaster position="top-right" />
+            <RouterProvider router={router} />
+          </AuthProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
